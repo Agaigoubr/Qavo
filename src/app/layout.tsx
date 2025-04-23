effaf8b784591f9bb44d1079bc166e9e1fd5ff3b
Agaigoubr <agaigoubrhim@gmail.com>
2025-04-23 18:58:51 +0100

    تحديث الكود وإصلاح الأخطاء

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navber from "@/components/Navber/page";
+import Footer from "@/components/Footer/page";
+import Test from "@/components/Testnavber/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+             <Test />
+
         {children}
+<Footer />
+        
       </body>
     </html>
   );
